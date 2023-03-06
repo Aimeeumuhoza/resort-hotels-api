6b4920c6347f423a09bc29c0afd38689a5466d97
Aimeeumuhoza <aimeeumuhoza1@gmail.com>
2023-03-06 17:25:35 +0200

    validatite data

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,10 +2,13 @@ const express = require("express")
 const  userController = require("../controllers/userController")
 const verifyEmail= require("../middleware/verifyEmail")
 //const { verifyTokenAndAdmin,verifyTokenAndUser, verifyToken } = require("../middleware/verifyToken")
+const validate=require("../middleware/validation")
+const registerschema=require("../validation/validate")
+
 
 const userRoute = express()
 
-userRoute.post("/create", userController.register)
+userRoute.post("/create", validate(registerschema),userController.register)
 userRoute.post("/login",verifyEmail,userController.login)
 userRoute.post("/forgotpsw",userController.forgotPassword)
 userRoute.post("/resetpassword/:token",userController.resetPassword)
---
+++ validation/validate.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+const { joiPasswordExtendCore } = require('joi-password');
+const joiPassword = Joi.extend(joiPasswordExtendCore);
+
+const registerschema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(30).required().label("name"),
+    email: Joi.string().lowercase().required().label("email"),
+    password:joiPassword
+    .string()
+    .minOfSpecialCharacters(2)
+    .minOfLowercase(2)
+    .minOfUppercase(2)
+    .minOfNumeric(2)
+    .noWhiteSpaces()
+    .required(),
+});
+
+module.exports = registerschema
