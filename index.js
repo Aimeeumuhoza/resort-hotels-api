d82b9952ea75327225a78cfba25ebb21b85a5842
Aimeeumuhoza <aimeeumuhoza1@gmail.com>
2023-05-24 22:26:18 +0200

    swagger documantation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const dotenv = require("dotenv")
+const  morgan =require ("morgan");
+const swaggerUI = require ("swagger-ui-express")
+const docs =require ("./swagger.json");
 dotenv.config()
 const dataB = require("./database/dataB")
 const userRoute = require("./routes/userRoute")
@@ -9,10 +12,12 @@ const commentRoute = require('./routes/commentRoute')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('dev'));
 app.use("/user",userRoute)
 app.use("/hotel",hotelRoute)
 app.use("/room",roomRoute)
 app.use("/comment",commentRoute)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs));
 const port =  process.env.PORT;
 dataB()
 // function used to connect servers
